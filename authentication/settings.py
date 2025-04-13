bc8289d20c4790a7e627dc6d69323f8ff8ce3b5a
rakabimar <rakabimaghaniendra22@gmail.com>
2025-04-13 18:48:34 +0700

    [Fix]: Fix api url

--- authentication/settings.py
+++ authentication/settings.py
@@ -166,7 +166,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:8001", 
-    "http://localhost:8002"
+    "http://localhost:8002",
+    "http://localhost:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
